fe1df0028408edd02fb5e4728d3bebccc51fc5e8
Sofia Katzman <katzmansof@gmail.com>
2024-02-23 14:53:24 -0500

    define script flow

--- Banner Mockups/BannerMockupScript.jsx
+++ Banner Mockups/BannerMockupScript.jsx
@@ -8,10 +8,22 @@ FLOW:
 - designer creates folder as required 
 - designer opens photoshop and runs script
 - A dialog box asks the user to select required folder
-- ? A dialog box asks the user to use a checkbox to select the mockups being required * 
+- A dialog box asks the user to use a checkbox to select the mockups being required 
+- An object is created that saves the selected options 
 - The dialog then asks the user for a Hex color code for the background color (necessary for the collection page hero assets)
-- PSD creates mockups for each category. 
-- Mockups are saved in the output folder 
+- PSD creates mockups for each category: 
+
+        Iterate through the selected options object to call each necessary function
+        each function has it's own variation of this flow: 
+        ----------
+        PSD opens mockup X (from mockup folder)
+        PSD creates a new layer 
+        PSD inserts asset x (from asset folder)
+        PSD resizes asset to y 
+        PSD repositions asset to (x, y)
+        PSD saves file in mockup folder 
+        ----------
+
 - Dialog box confirms complete - user can click ok to exit out. 
 - User can now use mockups as necessary
 
