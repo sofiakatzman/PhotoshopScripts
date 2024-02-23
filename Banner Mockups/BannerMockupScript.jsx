a0967ca709804369d8ce081a9a4b997b8657c198
Sofia Katzman <katzmansof@gmail.com>
2024-02-23 14:45:59 -0500

    add psudo code for flow

---
+++ Banner Mockups/BannerMockupScript.jsx
@@ -0,0 +1,18 @@
+/* At time of script launch a few assumptions are made: 
+
+- File structure is as required - as detailed in README.md 
+- The designer has the color code for the background color handy.
+- ? The designer knows what mockups are needed
+
+FLOW: 
+- designer creates folder as required 
+- designer opens photoshop and runs script
+- A dialog box asks the user to select required folder
+- ? A dialog box asks the user to use a checkbox to select the mockups being required * 
+- The dialog then asks the user for a Hex color code for the background color (necessary for the collection page hero assets)
+- PSD creates mockups for each category. 
+- Mockups are saved in the output folder 
+- Dialog box confirms complete - user can click ok to exit out. 
+- User can now use mockups as necessary
+
+*/
